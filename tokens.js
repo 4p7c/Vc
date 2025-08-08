afcda950f337dd9f9cf67f6d6844bdaa5cd9348f
Ronny <106476177+4p7c@users.noreply.github.com>
2025-08-08 23:56:04 +0400

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,35 +1,17 @@
-// status can be "online", "idle", "dnd", or "invisible" or "offline"
-
 export default [
-
     {
-
-        channelId: "1402895543742763131",
-
+        channelId: "1403466465092567090",
         serverId: "1237419554167197817",
-
-        token: process.env.TOKEN,
-
+        token: "token", // put your bot token here directly
         selfDeaf: false,
-
         autoReconnect: {
-
             enabled: true,
-
             delay: 5, // ثواني
-
             maxRetries: 5,
-
         },
-
         presence: {
-
-            status: "dnd",
-
+            status: "dnd", // "online", "idle", "dnd", "invisible", or "offline"
         },
-
         selfMute: true,
-
     },
-
 ];
